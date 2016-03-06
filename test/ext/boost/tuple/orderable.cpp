32e3a28c5b55758301a0ec0329980b814c290b5d
Louis Dionne <ldionne.2@gmail.com>
2016-03-06 16:40:06 -0500

    keep only transitivity tests

--- test/ext/boost/tuple/orderable.cpp
+++ test/ext/boost/tuple/orderable.cpp
@@ -21,39 +21,6 @@ void Comparable_tests(Xs xs) {
     using hana::test::iff;
     using hana::test::implies;
 
-    hana::test::foreach2(xs, [](auto a, auto b) {
-
-        // reflexivity
-        BOOST_HANA_CHECK(
-            hana::equal(a, a)
-        );
-
-        // symmetry
-        BOOST_HANA_CHECK(
-            hana::equal(a, b) ^implies^ hana::equal(b, a)
-        );
-
-        // `not_equal` is the negation of `equal`
-        BOOST_HANA_CHECK(
-            hana::not_equal(a, b) ^iff^ hana::not_(hana::equal(a, b))
-        );
-
-        // equal.to and not_equal.to
-        BOOST_HANA_CHECK(
-            hana::equal.to(a)(b) ^iff^ hana::equal(a, b)
-        );
-
-        BOOST_HANA_CHECK(
-            hana::not_equal.to(a)(b) ^iff^ hana::not_equal(a, b)
-        );
-
-        // comparing
-        hana::test::_injection<0> f{};
-        BOOST_HANA_CHECK(
-            hana::comparing(f)(a, b) ^iff^ hana::equal(f(a), f(b))
-        );
-    });
-
     // transitivity
     hana::test::foreach3(xs, [](auto a, auto b, auto c) {
         BOOST_HANA_CHECK(
@@ -62,38 +29,6 @@ void Comparable_tests(Xs xs) {
         );
     });
 
-
-
-    constexpr auto list = hana::make<S>;
-    BOOST_HANA_CONSTANT_CHECK(hana::equal(
-        list(),
-        list()
-    ));
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::equal(
-        list(eq<0>{}),
-        list()
-    )));
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::equal(
-        list(),
-        list(eq<0>{})
-    )));
-    BOOST_HANA_CONSTANT_CHECK(hana::equal(
-        list(eq<0>{}),
-        list(eq<0>{})
-    ));
-
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::equal(
-        list(eq<0>{}, eq<1>{}),
-        list(eq<0>{})
-    )));
-    BOOST_HANA_CONSTANT_CHECK(hana::equal(
-        list(eq<0>{}, eq<1>{}),
-        list(eq<0>{}, eq<1>{})
-    ));
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::equal(
-        list(eq<0>{}, eq<1>{}, eq<2>{}, eq<3>{}),
-        list(eq<0>{}, eq<1>{}, eq<2>{}, eq<4>{})
-    )));
 }
 
 
@@ -106,44 +41,6 @@ void Orderable_tests(Xs xs) {
     using hana::test::implies;
     using hana::test::iff;
 
-    hana::test::foreach2(xs, [](auto a, auto b) {
-        // antisymmetry
-        BOOST_HANA_CHECK(
-            hana::and_(hana::less_equal(a, b), hana::less_equal(b, a))
-                ^implies^ hana::equal(a, b)
-        );
-
-        // totality
-        BOOST_HANA_CHECK(
-            hana::or_(hana::less_equal(a, b), hana::less_equal(b, a))
-        );
-
-        // other methods in terms of `less_equal`
-        BOOST_HANA_CHECK(
-            hana::less(a, b) ^iff^ hana::not_(hana::less_equal(b, a))
-        );
-
-        BOOST_HANA_CHECK(
-            hana::greater(a, b) ^iff^ hana::less(b, a)
-        );
-
-        BOOST_HANA_CHECK(
-            hana::greater_equal(a, b) ^iff^ hana::not_(hana::less(a, b))
-        );
-
-        // less.than & al.
-        BOOST_HANA_CHECK(hana::less.than(a)(b) ^iff^ hana::less(b, a));
-        BOOST_HANA_CHECK(hana::greater.than(a)(b) ^iff^ hana::greater(b, a));
-        BOOST_HANA_CHECK(hana::less_equal.than(a)(b) ^iff^ hana::less_equal(b, a));
-        BOOST_HANA_CHECK(hana::greater_equal.than(a)(b) ^iff^ hana::greater_equal(b, a));
-
-        // ordering
-        hana::test::_injection<0> f{}; // test::_injection is also monotonic
-        BOOST_HANA_CHECK(
-            hana::ordering(f)(a, b) ^iff^ hana::less(f(a), f(b))
-        );
-    });
-
     // transitivity
     hana::test::foreach3(xs, [](auto a, auto b, auto c) {
         BOOST_HANA_CHECK(
@@ -151,45 +48,6 @@ void Orderable_tests(Xs xs) {
                 ^implies^ hana::less_equal(a, c)
         );
     });
-
-
-
-    constexpr auto list = hana::make<S>;
-
-    //////////////////////////////////////////////////////////////////
-    // less
-    //////////////////////////////////////////////////////////////////
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::less(
-        list(),
-        list()
-    )));
-
-    BOOST_HANA_CONSTANT_CHECK(hana::less(
-        list(),
-        list(invalid{})
-    ));
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::less(
-        list(invalid{}),
-        list()
-    )));
-    BOOST_HANA_CONSTANT_CHECK(hana::less(
-        list(ord<0>{}),
-        list(ord<7>{})
-    ));
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::less(
-        list(ord<1>{}),
-        list(ord<0>{})
-    )));
-
-    BOOST_HANA_CONSTANT_CHECK(hana::not_(hana::less(
-        list(ord<0>{}, ord<1>{}, ord<8>{}),
-        list(ord<0>{}, ord<1>{})
-    )));
-
-    BOOST_HANA_CONSTANT_CHECK(hana::less(
-        list(ord<0>{}, ord<0>{}, ord<8>{}),
-        list(ord<0>{}, ord<1>{})
-    ));
 }
 
 
